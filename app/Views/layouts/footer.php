591c96a48a6e166e21256796fd284a563f03fad9
Herry Sutiana Subagja <herriss890@gmail.com>
2020-09-10 13:46:47 +0700

    first commit

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,81 @@
+<?php
+if (isset($grafik)) {
+    foreach ($grafik as $data) {
+        $total[] = $data['total'];
+        $month[] = $data['month'];
+    }
+}
+?>
+
+<aside class="control-sidebar control-sidebar-dark">
+    <div class="p-3">
+        <h5>Title</h5>
+        <p>Sidebar content</p>
+    </div>
+</aside>
+
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-inline">
+        Sistem Informasi eCommerce
+    </div>
+    <strong>Copyright © Subagja Developer <a href="<?php echo base_url('/'); ?>">Subagja Corp</a>.</strong> H.A Integrated
+</footer>
+</div>
+<script src="<?php echo base_url('asseet'); ?>/js/jquery-3.5.1.min.js"></script>
+<script src="<?php echo base_url('themes/plugins'); ?>/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url('themes/dist'); ?>/js/adminlte.min.js"></script>
+<script src="<?php echo base_url('asseet'); ?>/js/jquery.dataTables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        // $('#example').DataTable();
+        $('#dttable').DataTable();
+    });
+</script>
+<script src="<?php echo base_url('themes/dist'); ?>/js/filter.js"></script>
+<?php if (isset($grafik)) { ?>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+    <script>
+        var chart = document.getElementById("myChart").getContext('2d');
+        var areaChart = new Chart(chart, {
+            type: 'bar',
+            data: {
+                labels: <?php echo json_encode($month); ?>,
+                datasets: [{
+                    label: "Penjualan bulan ini",
+                    data: <?php echo json_encode($total); ?>,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.5)',
+                        'rgba(54, 162, 253, 0.5)',
+                        'rgba(255, 206, 86, 0.5)',
+                        'rgba(184, 7, 7, 0.5)',
+                        'rgba(0, 17, 255, 0.5)',
+                        'rgba(255, 159, 64, 0.5)',
+                        'rgba(102, 225, 0, 0.5)',
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 253, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(184, 7, 7, 1)',
+                        'rgba(0, 17, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(102, 225, 0, 1)',
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    </script>
+<?php } ?>
+</body>
+
+</html>
